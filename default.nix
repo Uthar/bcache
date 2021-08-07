fe5ba86f2ffec290c50c2c67bb0af6332be3587c
kpg <kpg@posteo.net>
2021-08-07 11:58:25 +0000

    Fix src in default.nix
    FossilOrigin-Name: 3348bc45606a148c4a04f4377d4a9da1f91437a341fb57e024325defcc7d5621

--- default.nix
+++ default.nix
@@ -2,8 +2,12 @@ with import <nixos> {};
 
 stdenv.mkDerivation {
   pname="bcache";
-  version="0.1.0";
-  src=./.;
+  version="0.1.0-f8518f700b";
+  src= fetchfossil {
+    url = "https://fossil.galkowski.xyz/bcache";
+    rev = "f8518f700b683a5ec5beb870386816d2b1fd3a036a6aad23afb3c50edccb6108";
+    sha256 = "0z9mq60xzw4l08g1bzniqlba2rqaq2f20wfjfd1a2irvkdp2f8w1";
+  };
   buildInputs = [ makeWrapper nixUnstable.dev boost.dev nlohmann_json ];
   installPhase=''
     mkdir -pv $out/bin
