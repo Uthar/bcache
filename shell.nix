9891d04cd61da3c04cb7772c7f2ae2dee00cc704
kpg <kpg@posteo.net>
2022-04-06 13:49:54 +0000

    fix shell nix
    FossilOrigin-Name: 279c3483fab36746cd19664446e665e4cf9cf068f3c1840cf17073dea75ffd3e

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
-# worked for sure with commit d4590d21006
-with import <nixos> {};
+with import (builtins.fetchTarball {
+  url = "https://github.com/nixos/nixpkgs/archive/21.11.tar.gz";
+  sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+}) {};
 
 mkShell {
   buildInputs = [ gcc nixUnstable.dev boost.dev nlohmann_json ];
